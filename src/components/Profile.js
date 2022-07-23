db05e5b052602a90ccd10a9fbfe4df44939beae0
chrisphine <onyangoketta@gmail.com>
2022-07-23 19:07:34 +0300

    pass usecontex hook to profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React ,{useContext}from "react";
 import Interests from "./Interests";
+import { UserContex  } from "..context/User";
 
 function Profile({ user, theme }) {
+  const user=UserContex(useContext);
+  console.log(user);
+  
   if (!user) return <h2>Please Login To View Profile</h2>;
   return (
     <div>
